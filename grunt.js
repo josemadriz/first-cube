724a6e62ec1ee075ba2c1f4b61ac16469ba4f1a4
Leon Coto <leoncoto@gmail.com>
2012-11-22 10:23:26 +0000

    Grunt live-reload working now.

--- grunt.js
+++ grunt.js
@@ -9,73 +9,26 @@ module.exports = function(grunt) {
       options: {
         browser: true
       }
+    },
+    server: {
+      port: 8000,
+      base: 'www'
+    },
+    reload: {
+      port: 6001,
+      proxy: {
+        host: 'localhost'
+      }
+    },
+    watch: {
+      files: ['www/**/*'],
+      tasks: 'reload'
     }
   });
 
-  // Load tasks from "grunt-sample" grunt plugin installed via Npm.
   grunt.loadNpmTasks('grunt-sample');
-
-  // Default task.
-  grunt.registerTask('default', 'lint sample');
   grunt.loadNpmTasks('grunt-volo');
+  grunt.loadNpmTasks('grunt-reload');
+  grunt.registerTask('default', 'lint sample server');
 
 };
-// /*global module:false*/
-// module.exports = function(grunt) {
-
-//   // Project configuration.
-//   grunt.initConfig({
-//     pkg: '<json:package.json>',
-//     meta: {
-//       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-//         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-//         '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-//         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-//         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-//     },
-//     lint: {
-//       files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
-//     },
-//     qunit: {
-//       files: ['test/**/*.html']
-//     },
-//     concat: {
-//       dist: {
-//         src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
-//         dest: 'dist/<%= pkg.name %>.js'
-//       }
-//     },
-//     min: {
-//       dist: {
-//         src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-//         dest: 'dist/<%= pkg.name %>.min.js'
-//       }
-//     },
-//     watch: {
-//       files: '<config:lint.files>',
-//       tasks: 'lint qunit'
-//     },
-//     jshint: {
-//       options: {
-//         curly: true,
-//         eqeqeq: true,
-//         immed: true,
-//         latedef: true,
-//         newcap: true,
-//         noarg: true,
-//         sub: true,
-//         undef: true,
-//         boss: true,
-//         eqnull: true,
-//         browser: true
-//       },
-//       globals: {}
-//     },
-//     uglify: {}
-//   });
-
-//   // Default task.
-//   grunt.registerTask('default', 'lint qunit concat min');
-//   grunt.loadNpmTasks('grunt-volo');
-
-// };
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -3,9 +3,16 @@ define(function (require) {
     var MainApp = Backbone.View.extend({
       initialize: function () {
         console.log(this.options.message);
+        this.render();
+      },
+      render: function () {
+        this.$el.css('background-color', 'yellow');
+        this.$('h1').css('background-color', 'red');
+        return this;
       }
     });
     new MainApp({
-      message: 'Hello world!'
+      message: 'Hello world!',
+      el: 'body'
     });
 });
